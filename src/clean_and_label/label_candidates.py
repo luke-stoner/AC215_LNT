d374b00eb93bd156ed0d593889c32006e24581e6
Andrew Sullivan <andrewsullivan@dhcp-10-250-253-71.harvard.edu>
2023-09-26 19:28:00 -0400

    modified labeled file to work with GCP

--- src/clean_and_label/label_candidates.py
+++ src/clean_and_label/label_candidates.py
@@ -1,6 +1,22 @@
 import re
 import csv
+from google.cloud import storage 
+import os
+import nltk
+nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+##create GCP Client
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/andrewsullivan/Desktop/ac215-400221-4d622ff5cd5c.json"
+source_filename = f'unlabeled.csv'
+storage_client = storage.Client()
+bucket_name = "milestone2bucket"
+bucket = storage_client.bucket(bucket_name)
+in_file = bucket.blob(source_filename)
+# Download the file from GCS to your local machine
+in_file.download_to_filename(source_filename)
+
 
 #define function that shortens text to between 50 to 100 words, cropping at end of sentence if possible
 def crop_text(text):
@@ -24,7 +40,7 @@ def crop_text(text):
 """
 TODO: import unlabeled file from google cloud to clean and label scraped mentions of each candidate
 """
-with open(f'unlabeled.csv', 'r', newline='') as unlabled:
+with open(source_filename, 'r', newline='') as unlabled:
     csv_reader = csv.reader(unlabled)
     #create list of each mentions
     mentions = []
@@ -37,7 +53,7 @@ labeled = []
 #iterate through each mention in unlabeled file
 for mention in mentions:
     #get cropped text
-    cropped_mention = crop_text(mention[2])
+    cropped_mention = crop_text(mention[-1])
     
     #initialize VADER and evaluate sentiment
     analyzer = SentimentIntensityAnalyzer()
@@ -53,7 +69,7 @@ for mention in mentions:
         label = 1
 
     #write final new row with cropped text and label and append to labeled list
-    new_mention = [mention[0], mention[1], mention[2], mention[3], mention[3], cropped_mention, label]
+    new_mention = [mention[0], mention[1], mention[2], mention[3], mention[4], cropped_mention, label]
     labeled.append(new_mention)
 
 """
@@ -64,3 +80,6 @@ with open(outfilepath, 'w', newline='') as outfile:
     csv_writer = csv.writer(outfile)
     for entry in labeled:
         csv_writer.writerow(entry)
+
+out_file = bucket.blob(outfilepath)
+out_file.upload_from_filename(outfilepath)
