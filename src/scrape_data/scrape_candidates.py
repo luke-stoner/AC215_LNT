ce4f3c6a935f7f5707e59c079088c63f93c34d52
jportik <90353830+jportik@users.noreply.github.com>
2023-10-08 23:25:57 -0400

    Updated scraping for milestone3: modularized, error resistent and not hardcoded code

--- src/scrape_data/scrape_candidates.py
+++ src/scrape_data/scrape_candidates.py
@@ -3,10 +3,22 @@ import os, re, time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-def clean_text(text: str, max_words: int=100):
+
+# Define global variables
+START_DATE = '2023-09-17'
+END_DATE = '2023-09-20'
+MAXIMUM_NUMBER_WORDS = 100
+CLIP_AT_N = 50
+NUMBER_OF_TRIES = 3
+SCRAPE_SLEEP_TIME = 5
+RUN_HEADLESS = False
+
+
+def clean_text(text: str, max_words: int=MAXIMUM_NUMBER_WORDS):
     '''
-    Remove irrelevant characters and clip text at 50 words at the next sentence.
+    Remove irrelevant characters and clip text at MAXIMUM_NUMBER_WORDS words at the next sentence.
 
     Input:
     - text: str = input text from Internet Archive.
@@ -23,10 +35,10 @@ def clean_text(text: str, max_words: int=100):
     # clip text at 50 words to the next complete sentence
     words = text.split()
     n_words = len(words)
-    if n_words <= 50:
+    if n_words <= CLIP_AT_N:
         return text
     else:
-        for i in range(50, min(n_words, max_words)):
+        for i in range(CLIP_AT_N, min(n_words, max_words)):
             punc = bool(re.search(r'[.!?]', words[i]))
             if not punc is False:
                 break
@@ -43,7 +55,15 @@ def fetch_results(first_name: str, last_name: str):
     # initialize driver
     # options = webdriver.ChromeOptions()
     # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    if(RUN_HEADLESS):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(options=options)
+    else:
+        driver = webdriver.Chrome()
 
     # internet archive url
     url = f'https://archive.org/details/tv?q="{first_name}+{last_name}"&and%5B%5D=publicdate%3A%5B{START_DATE}+TO+{END_DATE}%5D&page=1'
@@ -57,18 +77,31 @@ def fetch_results(first_name: str, last_name: str):
     # scroll to bottom of webpage to prompt infinite scrolling and append further results
     at_bottom = 0
     len_old_results = 0
-    while at_bottom < 3: # 3 tries to generate more results to accomate for slow load times
-        time.sleep(5) # load time for each scroll attempt
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        len_old_results = len(results)
-        results = driver.find_elements(By.CLASS_NAME, 'item-ia.hov')
-        if len(results) > len_old_results:
-            at_bottom = 0
-        else:
-            at_bottom += 1
 
-    # drop first result (metadata)
-    return results[1:]
+    try:
+        while at_bottom < NUMBER_OF_TRIES: # 3 tries to generate more results to accomate for slow load times
+            time.sleep(SCRAPE_SLEEP_TIME) # load time for each scroll attempt
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            len_old_results = len(results)
+            results = driver.find_elements(By.CLASS_NAME, 'item-ia.hov')
+            if len(results) > len_old_results:
+                at_bottom = 0
+            else:
+                at_bottom += 1
+        # drop first result (metadata)
+        return results[1:]
+    
+    # Catch various exceptions that might occur during scaping
+    except NoSuchElementException:
+        print("The element was not found on the page!")
+    except TimeoutException:
+        print("The operation timed out!")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        driver.quit()
+
+    
 
 def upload_to_GCP(file_path: str):
     '''
@@ -78,7 +111,7 @@ def upload_to_GCP(file_path: str):
     file_path: str = path to file to upload.
     '''
     # load credentials
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/secrets/credentials.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/secrets/gcp_key.json"
 
     # connect to GCP
     bucket_name = 'data'
@@ -108,7 +141,7 @@ def scrape():
         last_name = row['last_name']
         party = row['party']
 
-        # fetch results from internet archive
+        # fetch results from internet archive, handled in separate function
         results = fetch_results(first_name, last_name)
 
         for result in results: 
