c16560ddf2939c923511cfd60dcf6d543083d2c6
Luke Stoner <lukestoner@g.harvard.edu>
2023-11-16 17:57:47 -0500

    Remove unnecessary frontend components

--- frontend/src/app/AppRoutes.js
+++ frontend/src/app/AppRoutes.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from "../components/Home";
-import Error404 from '../components/Error/404';
-import Experiments from '../components/Experiments';
-import Currentmodel from '../components/Currentmodel';
 
 const AppRouter = (props) => {
 
@@ -13,9 +10,6 @@ const AppRouter = (props) => {
     <React.Fragment>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/experiments" exact component={Experiments} />
-        <Route path="/currentmodel" exact component={Currentmodel} />
-        <Route component={Error404} />
       </Switch>
     </React.Fragment>
   );
